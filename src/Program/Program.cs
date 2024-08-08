3a4ac9438fea7e85ba45fcaf59c20e123be64373
Leandro Pereira <leandronobper@gmail.com>
2024-08-08 20:51:06 -0300

    Comentario agregado

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -3,6 +3,7 @@ Console.WriteLine(Substraction.Substract(10, 1));
 Console.WriteLine(Multiplication.Multiply(3, 3));
 Console.WriteLine(Division.Divide(18, 2));
 
+// Esta clase implementa la operación Suma
 public class Addition
 {
     public static int Add(int a, int b)
@@ -11,6 +12,7 @@ public class Addition
     }
 }
 
+// Esta clase implementa la operación multiplicación
 public class Multiplication
 {
     public static int Multiply(int a, int b)
